4bc2c530286f0ba76b535e880268c8b605609fd2
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-07-25 19:34:33 +0300

    update core.js

--- src/js/lib/core.js
+++ src/js/lib/core.js
@@ -3,11 +3,26 @@ const $ = function(selector) {
 }
 
 $.prototype.init = function(selector) {
-	if (!selector) return this // можно ли if делать в одну строку
+	if (!selector) {
+		return this;
+	}
 
-	Object.assign(this, document.querySelectorAll(selector))
-	this.length = document.querySelectorAll(selector).length
-	return this
+	if (selector.tagName) {
+		this[0] = selector;
+		this.length = 1;
+		return this;
+	}
+
+	const el = document.querySelectorAll(selector)
+	
+	if (!el.length) {
+		console.error(`Elements with class ${selector} were not found`);
+		return;
+	}
+
+	Object.assign(this, el)
+	this.length = el.length;
+	return this;
 }
 
 $.prototype.init.prototype = $.prototype
