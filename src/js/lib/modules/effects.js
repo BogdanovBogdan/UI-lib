86a1802108dad696918d5b154d5c94bf7f90e25a
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-10-27 06:36:31 +0300

    added effect fadeToggle and parameter of displaying

--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -1,9 +1,10 @@
 import $ from '../core';
 
-$.prototype.fadeIn = function(duration) {
+$.prototype.fadeIn = function(duration, display) {
   for (let i = 0; i < this.length; i++) {
 
     const draw = (progress) => {
+      this[i].style.display = display || 'block';
       this[i].style.opacity = progress;
     }
 
@@ -18,10 +19,20 @@ $.prototype.fadeOut = function(duration) {
 
     const draw = (progress) => {
       this[i].style.opacity = 1 - progress;
+      if (progress === 1) this[i].style.display = 'none';
     }
 
     this.animation(duration, draw)
   }
 
   return this;
+}
+
+$.prototype.fadeToggle = function(duration, display) {
+  for (let i = 0; i < this.length; i++) {
+    if (window.getComputedStyle(this[i]).display === 'none') {
+      this.fadeIn(duration, display);
+    } else this.fadeOut(duration);
+
+  }
 }
