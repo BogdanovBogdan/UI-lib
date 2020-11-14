53bf0ed8ae6ed356cc2ef27a42875f7fd3c7cc13
BogdanovBogdan <bogdanov_bogdanv@mail.ru>
2020-11-14 13:44:37 +0300

    minor fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,2 @@
 import './lib/lib'
 import $ from './lib/core';
-
-// $('.add-attribute').click(function() {
-//   $(this).toggleAttr(`hello`, '1')
-// })
-
-// $('.remove-attribute').click(function() {
-//   $('.add-attribute').removeAttr('hello');
-// })
-
-// $('div').click(function(event) {
-//   event.stopPropagation();
-//   console.log($(this).index())
-// })
-
-console.log($('.need').siblings());
-
